5f0b3053b5ea1b0b0ee8047c7508952925feb4f1
Igor Tyshchenko <igor@tyshchenko.me>
2019-01-10 22:14:30 +1100

    Set testing environment / check stylelint

--- gulpfile.js
+++ gulpfile.js
@@ -1,85 +1,85 @@
-"use strict";
+'use strict';
 
-const gulp = require("gulp");
-const sass = require("gulp-sass");
-const plumber = require("gulp-plumber");
-const postcss = require("gulp-postcss");
-const posthtml = require("gulp-posthtml");
-const include = require("posthtml-include");
-const autoprefixer = require("autoprefixer");
-const minify = require("gulp-csso");
-const imagemin = require("gulp-imagemin");
-const rename = require("gulp-rename");
-const server = require("browser-sync").create();
-const del = require("del");
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const plumber = require('gulp-plumber');
+const postcss = require('gulp-postcss');
+const posthtml = require('gulp-posthtml');
+const include = require('posthtml-include');
+const autoprefixer = require('autoprefixer');
+const minify = require('gulp-csso');
+const imagemin = require('gulp-imagemin');
+const rename = require('gulp-rename');
+const server = require('browser-sync').create();
+const del = require('del');
+const rollup = require('gulp-better-rollup');
+const sourcemaps = require('gulp-sourcemaps');
 
-gulp.task("clean", clean);
-gulp.task("copy", copy);
-gulp.task("images", images);
-gulp.task("html", html);
-gulp.task("style", style);
+gulp.task('clean', () => del('build'));
 
-gulp.task(
-  "build",
-  gulp.series("clean", gulp.parallel("copy", "style", "html"))
+gulp.task('copy', () =>
+  gulp
+    .src([
+      'source/fonts/**/*.{woff,woff2}',
+      'source/img/**'
+    ], {
+      base: 'source'
+    })
+    .pipe(gulp.dest('build'))
 );
 
-gulp.task("serve", gulp.parallel(watchFiles, browserSync));
-
-function clean() {
-  return del("build");
-};
-
-function copy() {
-  return gulp
-    .src([
-      "source/fonts/**/*.{woff,woff2}",
-      "source/img/**",
-      "source/js/**"
-    ], {
-      base: "source"
-    })
-    .pipe(gulp.dest("build"));
-};
-
-function images() {
-  return gulp
-    .src("source/img/**/*.{png,jpg,svg}")
+gulp.task('images', () =>
+  gulp
+    .src('source/img/**/*.{png,jpg,svg}')
     .pipe(imagemin([
       imagemin.optipng({optimizationLevel: 3}),
       imagemin.jpegtran({progressive: true}),
       imagemin.svgo()
     ]))
-    .pipe(gulp.dest("source/img"));
-}
+    .pipe(gulp.dest('source/img'))
+);
 
-function style() {
-  return gulp
-    .src("source/sass/style.scss")
+gulp.task('html', () =>
+  gulp
+    .src('source/*.html')
+    .pipe(posthtml([
+      include()
+    ]))
+    .pipe(gulp.dest('build'))
+);
+
+gulp.task('style', () =>
+  gulp
+    .src('source/sass/style.scss')
     .pipe(plumber())
     .pipe(sass())
     .pipe(postcss([
       autoprefixer()
     ]))
-    .pipe(gulp.dest("build/css"))
+    .pipe(gulp.dest('build/css'))
     .pipe(minify())
-    .pipe(rename("style.min.css"))
-    .pipe(gulp.dest("build/css"));
-}
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('build/css'))
+);
 
-function html() {
-  return gulp
-    .src("source/*.html")
-    .pipe(posthtml([
-      include()
-    ]))
-    .pipe(gulp.dest("build"));
-};
+gulp.task('scripts', () =>
+  gulp
+    .src('source/js/main.js')
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+    .pipe(rollup({}, 'iife'))
+    .pipe(sourcemaps.write(''))
+    .pipe(gulp.dest('build/js'))
+);
+
+gulp.task('build', gulp.series('clean', gulp.parallel('copy', 'style', 'html', 'scripts')));
+
+gulp.task('serve', gulp.parallel(browserSync, watchFiles));
 
 function browserSync(done) {
   server.init({
     server: {
-      baseDir: "build/"
+      baseDir: 'build/'
     },
   port: 3000
   });
@@ -92,6 +92,7 @@ function browserSyncReload(done) {
 }
 
 function watchFiles() {
-  gulp.watch("source/sass/**/*.{scss,sass}", gulp.series(style, browserSyncReload));
-  gulp.watch("source/*.html", gulp.series(html, browserSyncReload));
+  gulp.watch('source/sass/**/*.{scss,sass}', gulp.series('style', browserSyncReload));
+  gulp.watch('source/*.html', gulp.series('html', browserSyncReload));
+  gulp.watch('source/js/**/*.js', gulp.series('scripts', browserSyncReload));
 }
